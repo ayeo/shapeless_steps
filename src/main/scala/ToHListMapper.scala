9bfdf2c04c75e1ed7e613ccace32eafe7d418767
Michał Grabowski <michal.grabowski@softwaremill.com>
2023-09-09 16:43:37 +0200

    Remove implicit converters

--- src/main/scala/ToHListMapper.scala
+++ src/main/scala/ToHListMapper.scala
@@ -1,52 +1,12 @@
 package pl.ayeo.steps
 
-import shapeless.{::, HList, HNil}
+import shapeless.{::, HNil}
 
 object ToHListMapper {
-
-  trait Converters[A] {
-    type Out <: HList
-    def toHList(a: A): Out
-  }
-
-  private type Aux[A, Out0 <: HList] = Converters[A] {type Out = Out0}
-
-  def apply[A](implicit ev: Converters[A]): Aux[A, ev.Out] = ev
-
-  implicit val nameAndAnimalToHList: Aux[NameAndAnimal, String :: Empty.type :: Boolean :: HNil] =
-    new Converters[NameAndAnimal] {
-      type Out = String :: Empty.type :: Boolean :: HNil
-
-      def toHList(na: NameAndAnimal): Out = na.name :: Empty :: na.animal :: HNil
-    }
-
-  implicit val ageAndAnimalToHList: Aux[AgeAndAnimal, Empty.type :: Int :: Boolean :: HNil] =
-    new Converters[AgeAndAnimal] {
-      type Out = Empty.type :: Int :: Boolean :: HNil
-
-      def toHList(na: AgeAndAnimal): Out = Empty :: na.age :: na.animal :: HNil
-    }
-
-  implicit val nameAndAnimalToHList2: Aux[NameAndAge, String :: Int :: Empty.type :: HNil] =
-    new Converters[NameAndAge] {
-      type Out = String :: Int :: Empty.type :: HNil
-
-      def toHList(na: NameAndAge): Out = na.name :: na.age :: Empty :: HNil
-    }
-
-  implicit val nameAndAgeAndAnimalToHList: Aux[NameAndAgeAndAnimal, String :: Int :: Boolean :: HNil] =
-    new Converters[NameAndAgeAndAnimal] {
-      type Out = String :: Int :: Boolean :: HNil
-
-      def toHList(na: NameAndAgeAndAnimal): Out = na.name :: na.age :: na.animal :: HNil
-    }
-
-  private def convertToHList[A <: Subject](input: A)(implicit ev: Converters[A]): ev.Out = ev.toHList(input)
-
   def process(s: Subject): Serializable :: Any :: Any :: HNil = s match {
-    case n: NameAndAnimal => convertToHList(n)
-    case n: NameAndAge => convertToHList(n)
-    case n: NameAndAgeAndAnimal => convertToHList(n)
-    case n: AgeAndAnimal => convertToHList(n)
+    case na: NameAndAnimal =>  na.name :: Empty :: na.animal :: HNil
+    case na: NameAndAge => na.name :: na.age :: Empty :: HNil
+    case na: NameAndAgeAndAnimal => na.name :: na.age :: na.animal :: HNil
+    case na: AgeAndAnimal => Empty :: na.age :: na.animal :: HNil
   }
 }
--- src/main/scala/ToSubjectMapper.scala
+++ src/main/scala/ToSubjectMapper.scala
@@ -8,13 +8,9 @@ private object ToSubjectMapper {
   }
 
   def process(hlist: HList): Subject = hlist match {
-    case (name: String) :: (age: Int) :: Empty :: HNil =>
-      NameAndAge(name, age)
-    case (name: String) :: (age: Int) :: (hasAnimal: Boolean) :: HNil =>
-      NameAndAgeAndAnimal(name, age, hasAnimal)
-    case (name: String) :: Empty :: (hasAnimal: Boolean) :: HNil =>
-      NameAndAnimal(name, hasAnimal)
-    case Empty :: (age: Int) :: (hasAnimal: Boolean) :: HNil =>
-      AgeAndAnimal(age, hasAnimal)
+    case (name: String) :: (age: Int) :: Empty :: HNil => NameAndAge(name, age)
+    case (name: String) :: (age: Int) :: (hasAnimal: Boolean) :: HNil => NameAndAgeAndAnimal(name, age, hasAnimal)
+    case (name: String) :: Empty :: (hasAnimal: Boolean) :: HNil => NameAndAnimal(name, hasAnimal)
+    case Empty :: (age: Int) :: (hasAnimal: Boolean) :: HNil => AgeAndAnimal(age, hasAnimal)
   }
 }
